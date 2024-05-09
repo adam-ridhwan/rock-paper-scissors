cdcb87e31bce497792c32542ce648700108a7f0f
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-05-08 21:26:22 -0400

    rules

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -24,7 +24,9 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      `fixed inset-0 z-50 bg-black/80  
+      data-[state=open]:animate-in data-[state=closed]:animate-out 
+      data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0`,
       className
     )}
     {...props}
@@ -41,24 +43,24 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        `fixed left-[50%] top-[50%] z-50 flex h-full w-full max-w-lg translate-x-[-50%] 
-        translate-y-[-50%] justify-center gap-4 border bg-primary p-6 shadow-lg duration-200
-        data-[state=open]:animate-in data-[state=closed]:animate-out 
-        data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 
-        data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 
-        data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] 
-        data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] 
-        sm:rounded-lg`,
+        `fixed left-[50%] top-[50%] z-50 flex h-full w-full translate-x-[-50%] translate-y-[-50%] justify-center
+        gap-4 border bg-primary p-6 shadow-lg duration-200 data-[state=open]:animate-in 
+        data-[state=closed]:animate-out data-[state=closed]:fade-out-0 
+        data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 
+        data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 
+        data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 
+        data-[state=open]:slide-in-from-top-[48%] sm:h-fit 
+        sm:max-w-lg sm:rounded-lg`,
         className
       )}
       {...props}
     >
       {children}
       <DialogPrimitive.Close
-        className={`absolute bottom-28  left-1/2 -translate-x-[50%] rounded-sm opacity-70 
-        ring-offset-background transition-opacity hover:opacity-100
-        disabled:pointer-events-none 
-        data-[state=open]:bg-primary data-[state=open]:text-muted-foreground`}
+        className={`absolute bottom-28 right-1/2 h-fit w-fit translate-x-[50%] rounded-sm opacity-70 ring-offset-background 
+        transition-opacity hover:opacity-100 disabled:pointer-events-none
+        data-[state=open]:bg-primary data-[state=open]:text-muted-foreground 
+        sm:bottom-0 sm:right-5 sm:top-5 sm:translate-x-0`}
       >
         <CloseIcon />
         <span className='sr-only'>Close</span>
@@ -103,7 +105,7 @@ const DialogTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      'py-20 text-4xl font-semibold leading-none tracking-tight text-primary-foreground',
+      'py-20 text-4xl font-semibold leading-none tracking-tight text-primary-foreground sm:py-4',
       className
     )}
     {...props}
